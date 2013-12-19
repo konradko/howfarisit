367c5e346cff05a1a5f8a3a349d43d2ee2cf113c
konradko <konrad.korzel@gmail.com>
2013-12-19 22:00:07 +0000

    Add GeoIP for user location

--- main/views.py
+++ main/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.contrib.gis.utils import GeoIP
 
 from main.models import Distance
 
+# Helper function
+def get_user_location(request):
+    user_ip = request.META.get('REMOTE_ADDR', None)
+    return GeoIP().lon_lat(user_ip)
+
 def view_distance(request):
+
     user_distance = Distance(
         user_ip='ip',
         user_location='loc',
         dest_location='dest_loc',
-        distance='dummy distance'
+        distance=str(get_user_location(request))
         )
     return render(
         request,
