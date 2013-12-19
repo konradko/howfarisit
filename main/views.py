4c4ff32ab4a190e6895f74b1f80defa799c437ab
konradko <konrad.korzel@gmail.com>
2013-12-19 21:14:40 +0000

    Add view_distance with dummy query

--- main/views.py
+++ main/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render
+
+from main.models import Distance
+
+def view_distance(request):
+    user_distance = Distance(
+        user_ip='ip',
+        user_location='loc',
+        dest_location='dest_loc',
+        distance='dummy distance'
+        )
+    return render(
+        request,
+        'distance.html',
+        {'distance': user_distance}
+    )
