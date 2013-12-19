cda4a5067f85799412a876628883502e0e38adc9
konradko <konrad.korzel@gmail.com>
2013-12-19 20:35:56 +0000

    Add Distance model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Distance(models.Model):
+    user_ip = models.IPAddressField()
+    user_location = models.CharField()
+    dest_location = models.CharField()
+    distance = models.CharField()
+    visit_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.distance
