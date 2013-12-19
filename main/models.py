2a9c78f3bae9107ca480dcc2f85020b6fc74f145
konradko <konrad.korzel@gmail.com>
2013-12-19 21:12:17 +0000

    Add max_len to CharFields

--- main/models.py
+++ main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Distance(models.Model):
     user_ip = models.IPAddressField()
-    user_location = models.CharField()
-    dest_location = models.CharField()
-    distance = models.CharField()
+    user_location = models.CharField(max_length=250)
+    dest_location = models.CharField(max_length=250)
+    distance = models.CharField(max_length=250)
     visit_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
