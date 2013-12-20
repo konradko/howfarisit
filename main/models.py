d4cacbb10d3eea6a5d892b6d4db6423126a0934e
konradko <konrad.korzel@gmail.com>
2013-12-20 00:34:29 +0000

    Remove location fields, change distance to Decimal

--- main/models.py
+++ main/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class Distance(models.Model):
     user_ip = models.IPAddressField()
-    user_location = models.CharField(max_length=250)
-    dest_location = models.CharField(max_length=250)
-    distance = models.CharField(max_length=250)
+    distance = models.DecimalField(max_digits=999, decimal_places=999)
     visit_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
