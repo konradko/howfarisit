ab1be0ddc1f999fd7efd985a80b6e089d0d20153
konradko <konrad.korzel@gmail.com>
2013-12-23 01:03:43 +0000

    Add tests for model and views

--- main/tests.py
+++ main/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+import unittest
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 from django.test import TestCase
+from django.conf import settings
 
+from main.models import Distance
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestModel(TestCase):
+
+    def test_distance_creation(self):
+        distance = Distance(user_ip='127.0.0.1', distance=1)
+        self.assertTrue(isinstance(distance, Distance))
+        self.assertEqual(distance.__unicode__(), distance.distance)
+
+class TestViews(TestCase):
+
+    def test_view_distance(self):
+        url = reverse("main.views.view_distance")
+        response = Client().get(url)
+        # Assuming that tests are run on locally and
+        # tools.py/get_lon_lat uses different host as user address
+        if settings.DEBUG:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('km', response.content)
+        else:
+            self.assertEqual(response.status_code, 500)
+            self.assertIn("Sorry", response.content)
+
+    def test_api_view(self):
+        url = reverse("main.views.api_view")
+        response = Client().get(url)
+        # Assuming that tests are run on locally and
+        # tools.py/get_lon_lat uses different host as user address
+        if settings.DEBUG:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Distance', response.content)
+        else:
+            self.assertEqual(response.status_code, 500)
+            self.assertIn("Error", response.content)
+
+if __name__ == '__main__':
+    unittest.main()
